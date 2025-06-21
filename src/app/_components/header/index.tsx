1050ebd12df96bd90acee48ceff9452a0405e4d9
linhvodoiss <tangbang250820@gmail.com>
2025-06-21 22:23:51 +0700

    update logo

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -24,7 +24,13 @@ export default function Header({ token, user }: Props) {
     <WebHeaderStyled className='bg-background-primary border-primary-system border-b-2'>
       <div className='header__container mx-auto flex w-full max-w-[1920px] items-center justify-between pr-8 text-xl text-white'>
         <div className='flex items-center justify-start gap-8'>
-          <Image src='/images/logo.png' alt='logo' width={1024} height={1024} className='w-24 object-contain' />
+          <Image
+            src='/images/logo_transparent.png'
+            alt='logo'
+            width={1024}
+            height={1024}
+            className='w-24 object-contain'
+          />
           <ul className='flex items-center justify-center'>
             {menuItems.map(item => (
               <li key={item.label}>
